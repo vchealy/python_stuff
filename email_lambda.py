70add4a8f5a3497e81004cc54eef4fc9f8e80a7d
unknown <vchealy@gmai.com>
2019-08-03 23:08:09 +0100

    lambda email address

---
+++ email_lambda.py
@@ -0,0 +1,12 @@
+def email_address(
+    username, domain): return f'Email Address: {username}{domain}'
+
+# def email_address(
+#     username, domain): return f'Email Address: {username}{domain}'
+
+
+user1 = email_address('vchealy', '@gmail.com')
+user2 = email_address('vhealyv', '@gmail.com')
+
+print(user1)
+print(user2)
