8c12987f61c4c237c860513ee9dfb0df08db30ff
unknown <vchealy@gmai.com>
2019-08-04 09:50:20 +0100

    upupup

--- pytest_patch.py
+++ pytest_patch.py
@@ -1,7 +1,13 @@
 import secrets
+
+
 def gen_token():
     return f'TOKEN_{secrets.token_hex(8)}'
 
+
 def test_gen_key(monkeypatch):
-    monkeypatch.setattr('secrets.token_hex', lambda _:'feedfacecafebeef')
-    assert gen_token()== f"TOKEN_{'feedfacecafebeefd'}"
+    monkeypatch.setattr('secrets.token_hex', lambda _: 'feedfacecafebeef')
+    assert gen_token() == f"TOKEN_{'feedfacecafebeefd'}"
+
+
+print(gen_token)
