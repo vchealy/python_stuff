811b0be35897c03fc85b2a04cde43a4aad6988d2
unknown <vchealy@gmai.com>
2019-08-04 09:47:08 +0100

    patchy stuff

---
+++ pytest_patch.py
@@ -0,0 +1,7 @@
+import secrets
+def gen_token():
+    return f'TOKEN_{secrets.token_hex(8)}'
+
+def test_gen_key(monkeypatch):
+    monkeypatch.setattr('secrets.token_hex', lambda _:'feedfacecafebeef')
+    assert gen_token()== f"TOKEN_{'feedfacecafebeefd'}"
