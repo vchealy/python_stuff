89ffef31143e8f222df0768118bd5d1945d2a17c
Vincent Healy <vchealy@gmail.com>
2019-08-20 20:04:47 +0100

    Update email_lambda_two.py

--- email_lambda_two.py
+++ email_lambda_two.py
@@ -1,16 +1,12 @@
 user1_email = input('What is your username?')
 user2_email = input('What is your friends username?')
 
-
-def email_address(
-    username, domain): return f'Email Address: {username}{domain}'
-
-# def email_address(
-#     username, domain): return f'Email Address: {username}{domain}'
-
+email_address = lambda username, domain: f'Email Address: {username}{domain}'
 
 user1 = email_address(user1_email, '@gmail.com')
 user2 = email_address(user2_email, '@gmail.com')
+
+
 
 print(user1)
 print(user2)
