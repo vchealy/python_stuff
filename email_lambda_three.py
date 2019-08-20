a088f20f5d16af392b863f03024099333b5a623a
vchealy <vchealy@gmail.com>
2019-08-20 20:47:44 +0100

    lambda three update

--- email_lambda_three.py
+++ email_lambda_three.py
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
+# Use Terminal
+# Variables
 user1_email = input('What is your username?')
 user2_email = input('What is your friends username?')
-
 fixed_domain = '@gmail.com'
+
+# lambda expression
 email_address = lambda username, fixed_domain: f'Email Address: {username}{fixed_domain}'
 
+# function requests
 user1 = email_address(user1_email, fixed_domain)
 user2 = email_address(user2_email, fixed_domain)
 
-
+# outputs
 print(user1)
 print(user2)
-=======
-user1_email = input('What is your username?')
-user2_email = input('What is your friends username?')
-
-fixed_domain = '@gmail.com'
-email_address = lambda username, fixed_domain: f'Email Address: {username}{fixed_domain}'
-
-user1 = email_address(user1_email, fixed_domain)
-user2 = email_address(user2_email, fixed_domain)
-
-
-print(user1)
-print(user2)
->>>>>>> 525659ed4976c0bc023534a971ae1184386fe7f1
