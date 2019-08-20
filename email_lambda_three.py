525659ed4976c0bc023534a971ae1184386fe7f1
Vincent Healy <vchealy@gmail.com>
2019-08-20 20:41:00 +0100

    Add files via upload

--- email_lambda_three.py
+++ email_lambda_three.py
@@ -1,7 +1,6 @@
 user1_email = input('What is your username?')
 user2_email = input('What is your friends username?')
 
-<<<<<<< HEAD
 fixed_domain = '@gmail.com'
 email_address = lambda username, fixed_domain: f'Email Address: {username}{fixed_domain}'
 
@@ -9,14 +8,5 @@ user1 = email_address(user1_email, fixed_domain)
 user2 = email_address(user2_email, fixed_domain)
 
 
-=======
-email_address = lambda username, domain: f'Email Address: {username}{domain}'
-
-user1 = email_address(user1_email, '@gmail.com')
-user2 = email_address(user2_email, '@gmail.com')
-
-
-
->>>>>>> 89ffef31143e8f222df0768118bd5d1945d2a17c
 print(user1)
 print(user2)
