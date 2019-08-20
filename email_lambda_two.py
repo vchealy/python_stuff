4e7bf04cb77ceba4956caf9313889b82aaa98004
Vincent Healy <vchealy@gmail.com>
2019-08-20 19:59:15 +0100

    Update email_lambda_two.py

--- email_lambda_two.py
+++ email_lambda_two.py
@@ -1,4 +1,5 @@
 user1_email = input('What is your username?')
+user2_email = input('What is your friends username?')
 
 
 def email_address(
@@ -8,8 +9,8 @@ def email_address(
 #     username, domain): return f'Email Address: {username}{domain}'
 
 
-user1 = email_address('vchealy', '@gmail.com')
-user2 = email_address('vhealyv', '@gmail.com')
+user1 = email_address(user1_email, '@gmail.com')
+user2 = email_address(user2_email, '@gmail.com')
 
 print(user1)
 print(user2)
